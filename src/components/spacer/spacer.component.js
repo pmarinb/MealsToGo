df007d71c7db654e4e0682a1b6ecc50ed771d00f
Pablo Marin <p.marinb@alumnos.duoc.cl>
2022-07-11 20:55:09 -0400

    Fixing for android

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,9 +1,12 @@
-import styled from "styled-components/native";
+import React from "react";
+import styled, { useTheme } from "styled-components/native";
 
 const sizeVariant = {
   small: 1,
   medium: 2,
   large: 3,
+  xl: 4,
+  xxl: 5,
 };
 
 const positionVariant = {
@@ -17,12 +20,19 @@ const getVariant = (position, size, theme) => {
   const sizeIndex = sizeVariant[size];
   const property = positionVariant[position];
   const value = theme.space[sizeIndex];
+
   return `${property}:${value}`;
 };
 
-export const Spacer = styled.View`
-  ${({ position, size, theme }) => getVariant(position, size, theme)}
+const SpacerView = styled.View`
+  ${({ variant }) => variant};
 `;
+
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
 
 Spacer.defaultProps = {
   position: "top",
---
+++ src/features/restaurants/components/restaurant-info.card.js
@@ -0,0 +1,35 @@
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+export const Icon = styled.Image`
+  width: 15px;
+  height: 15px;
+`;
+
+export const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+export const RestaurantCardCover = styled(Card.Cover)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+export const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+export const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+export const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+export const Section = styled.View`
+  flex-direction: row;
+  aling-items: center;
+`;
+export const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
