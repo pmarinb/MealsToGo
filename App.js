57616792cacdf37a37166191f274a088efc8f42f
Pablo Marin <p.marinb@alumnos.duoc.cl>
2022-07-29 14:26:14 -0400

    Firebase installed

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { theme } from "./src/infrastructure/theme";
-
 import { Navigation } from "./src/infrastructure/navigation";
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -14,7 +16,35 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
+const firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    signInWithEmailAndPassword(auth, "email", "password")
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setIsAuthenticated(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   let [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
